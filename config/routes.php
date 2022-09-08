9656b461d59b2644d907e1d93fe5023ca4213d51
edualvesz <ea-hendrix@hotmail.com>
2022-09-07 21:39:10 -0300

    validating user and password verify

--- config/routes.php
+++ config/routes.php
@@ -6,6 +6,7 @@ use \Alura\Cursos\Controller\FormularioInsercao;
 use Alura\Cursos\Controller\FormularioLogin;
 use \Alura\Cursos\Controller\ListarCursos;
 use Alura\Cursos\Controller\Persistencia;
+use Alura\Cursos\Controller\RealizarLogin;
 
 return [
   '/novo-curso' => FormularioInsercao::class,
@@ -14,6 +15,7 @@ return [
   '/excluir-curso' => Exclusao::class,
   '/alterar-curso' => FormularioEdicao::class,
   '/login' => FormularioLogin::class,
+  '/realiza-login' => RealizarLogin::class,
 ];
 
 ?>
